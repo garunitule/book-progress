ed8fe4b44b9c6e269e681689c616ddd37ae181ed
garunitule <youtaiguhe@gmail.com>
2021-01-08 23:01:21 +0900

    不要なreturn削除

--- src/BookStates/State.tsx
+++ src/BookStates/State.tsx
@@ -6,26 +6,24 @@ type Props = {
   books: bookList[];
 };
 
-const StateList: React.FC<Props> = ({ books }) => {
-  const bookCards = books.map((book) => (
-    <Card key={book.id}>
-      <Card.Content>
-        <Card.Header>{book.name}</Card.Header>
-        <Card.Description>
-          {book.name}
-          の読書進捗は「
-          {book.state.name}
-          」です
-        </Card.Description>
-      </Card.Content>
-    </Card>
-  ));
-
-  return (
-    <>
-      {bookCards}
-    </>
-  );
-};
+const StateList: React.FC<Props> = ({ books }) => (
+  <>
+    {
+      books.map((book) => (
+        <Card key={book.id}>
+          <Card.Content>
+            <Card.Header>{book.name}</Card.Header>
+            <Card.Description>
+              {book.name}
+              の読書進捗は「
+              {book.state.name}
+              」です
+            </Card.Description>
+          </Card.Content>
+        </Card>
+      ))
+    }
+  </>
+);
 
 export default StateList;
