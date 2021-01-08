9958fdccbb8969cf400011d01447247a2468ca8d
garunitule <youtaiguhe@gmail.com>
2021-01-08 22:59:50 +0900

    本の状態をあらわすように変更

--- src/BookStates/State.tsx
+++ src/BookStates/State.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Icon } from 'semantic-ui-react';
+import { Card } from 'semantic-ui-react';
 import { bookList } from '../Books/BookList';
 
 type Props = {
@@ -11,17 +11,12 @@ const StateList: React.FC<Props> = ({ books }) => {
     <Card key={book.id}>
       <Card.Content>
         <Card.Header>{book.name}</Card.Header>
-        <Card.Meta>
-          <span className="date">Joined in 2015</span>
-        </Card.Meta>
         <Card.Description>
           {book.name}
-          is a musician living in Nashville.
+          の読書進捗は「
+          {book.state.name}
+          」です
         </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <Icon name="user" />
-        22 Friends
       </Card.Content>
     </Card>
   ));
